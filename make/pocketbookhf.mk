f60c23550b0e5352a6064ae40bcb4d024364d756
m-tky <takuya.mukai@gmail.com>
2026-09-06 14:38:58 +0900

    build: distinguish PocketBook HF packages

--- make/pocketbookhf.mk
+++ make/pocketbookhf.mk
@@ -1,1 +1,8 @@
 include make/pocketbook.mk
+
+# Keep hard-float packages distinct from the legacy PocketBook build. Besides
+# making the architecture visible to users, release assets must have unique
+# names because both variants are published together.
+PB_PACKAGE = koreader-pocketbookhf$(KODEDUG_SUFFIX)-$(VERSION).zip
+PB_PACKAGE_OTA = koreader-pocketbookhf$(KODEDUG_SUFFIX)-$(VERSION).tar.xz
+PB_PACKAGE_OLD_OTA = koreader-pocketbookhf$(KODEDUG_SUFFIX)-$(VERSION).targz
